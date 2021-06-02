be123da7f4a32561ee624727297b66fb1f0e39ae
Attila Kesztyus <attila.kesztyus@scolvo.com>
2021-06-02 18:44:34 +0200

    Add backend skeleton

---
+++ backend/src/main/java/hu/flowacademy/meteo/MeteoBackendApplication.java
@@ -0,0 +1,13 @@
+package hu.flowacademy.meteo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeteoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeteoBackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/hu/flowacademy/meteo/MeteoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.flowacademy.meteo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeteoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
