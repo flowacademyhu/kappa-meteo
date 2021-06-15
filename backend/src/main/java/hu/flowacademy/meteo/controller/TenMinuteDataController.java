bb3adccf027bc5b8979d603473f57074fa78c180
Vass Tamás <vass.tamas87@gmail.com>
2021-06-15 11:00:50 +0200

    tenmin ctrl/ser {Nándi}

--- backend/src/main/java/hu/flowacademy/meteo/controller/TenMinuteDataController.java
+++ backend/src/main/java/hu/flowacademy/meteo/controller/TenMinuteDataController.java
@@ -1,6 +1,7 @@
 package hu.flowacademy.meteo.controller;
 
 import hu.flowacademy.meteo.model.TenMinuteData;
+import hu.flowacademy.meteo.service.TenMinuteDataService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ backend/src/main/java/hu/flowacademy/meteo/service/TenMinuteDataService.java
@@ -0,0 +1,20 @@
+package hu.flowacademy.meteo.service;
+
+import hu.flowacademy.meteo.model.TenMinuteData;
+import hu.flowacademy.meteo.repository.TenMinuteDataRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class TenMinuteDataService {
+
+    private final TenMinuteDataRepository tenMinuteDataRepository;
+    public List<TenMinuteData> findAll() {
+        return tenMinuteDataRepository.findAll();
+    }
+}
