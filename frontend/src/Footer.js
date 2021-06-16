28ac5490eb3ce30e1c53a78c3e1338d24d6dc175
Judak Barnabas <barnajudak@gmail.com>
2021-06-16 11:03:21 +0200

    Style-compontents npm added and footer fixed with styled-components {Robi}

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -1,26 +1,20 @@
-var style = {
-  backgroundColor: '#c54b3c',
-  textAlign: 'center',
-  padding: '20px',
-  position: 'fixed',
-  left: '0',
-  bottom: '0',
-  height: '60px',
-  width: '100%',
-};
+import styled from 'styled-components';
 
-var phantom = {
-  display: 'block',
-  padding: '20px',
-  height: '60px',
-  width: '100%',
-};
+const Style = styled.div`
+  background-color: #c54b3c;
+  text-align: center;
+  padding: 20px;
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  height: 60px;
+  width: 100%;
+`;
 
 function Footer({ children }) {
   return (
     <div>
-      <div style={phantom} />
-      <div style={style}>{children}</div>
+      <Style>{children}</Style>
     </div>
   );
 }
