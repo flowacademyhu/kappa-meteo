8567e4712b1fe4fef429df52a572285ddd0d51af
Judak Barnabas <barnajudak@gmail.com>
2021-06-16 15:11:48 +0200

    Icons added to navbar {Robi}

--- frontend/src/Menu.js
+++ frontend/src/Menu.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './Menu.css';
 import { Link } from 'react-router-dom';
+import { FaMapMarkedAlt } from 'react-icons/fa';
+import { IoBarChartOutline } from 'react-icons/io5';
 
 export default function Menu() {
   return (
     <div className="navbar justify-content-center">
       <Link className="nav-link" to="/mapview">
-        Térképnézet
+        <FaMapMarkedAlt /> Térképnézet
       </Link>
       <Link className="nav-link" to="/historicdata">
-        Hisztorikus Adatok
+        <IoBarChartOutline /> Hisztorikus Adatok
       </Link>
     </div>
   );
