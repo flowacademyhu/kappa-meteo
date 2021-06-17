3c5762f3ed8eb854d6697bb888cef6dde7eb5a60
Judak Barnabas <barnajudak@gmail.com>
2021-06-17 12:03:13 +0200

    Color added to icons @fixo97

--- frontend/src/DashBoard.js
+++ frontend/src/DashBoard.js
@@ -1,43 +1,50 @@
-import React from "react";
-import { WiHumidity, WiThermometer } from "react-icons/wi";
-import { VscDashboard } from "react-icons/vsc";
+import React from 'react';
+import { WiHumidity, WiThermometer } from 'react-icons/wi';
+import { VscDashboard } from 'react-icons/vsc';
+import { IconContext } from 'react-icons';
 
 export default function Dashboard() {
   return (
-    <div class='row row-cols-1 row-cols-md-3 g-4'>
-      <div class='col'>
-        <div class='card h-100'>
-          <WiThermometer size={100} />
-          <div class='card-body'>
-            <h5 class='card-title'>Hömérséklet</h5>
-            <p class='card-text'>0 Celsius</p>
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+      <div class="col">
+        <div class="card h-100">
+          <IconContext.Provider value={{ color: '#c54b3c' }}>
+            <WiThermometer size={100} />
+          </IconContext.Provider>
+          <div class="card-body">
+            <h5 class="card-title">Hőmérséklet</h5>
+            <p class="card-text">0 Celsius</p>
           </div>
-          <div class='card-footer'>
-            <small class='text-muted'>Last updated 10 mins ago</small>
-          </div>
-        </div>
-      </div>
-      <div class='col'>
-        <div class='card h-100'>
-          <WiHumidity size={100} />
-          <div class='card-body'>
-            <h5 class='card-title'>Páratartalom</h5>
-            <p class='card-text'>0 %</p>
-          </div>
-          <div class='card-footer'>
-            <small class='text-muted'>Last updated 10 mins ago</small>
+          <div class="card-footer">
+            <small class="text-muted">Last updated 10 mins ago</small>
           </div>
         </div>
       </div>
-      <div class='col'>
-        <div class='card h-100'>
-          <VscDashboard size={100} />
-          <div class='card-body'>
-            <h5 class='card-title'>Szélerősség</h5>
-            <p class='card-text'>0 km/h</p>
+      <div class="col">
+        <div class="card h-100">
+          <IconContext.Provider value={{ color: '#c54b3c' }}>
+            <WiHumidity size={100} />
+          </IconContext.Provider>
+          <div class="card-body">
+            <h5 class="card-title">Páratartalom</h5>
+            <p class="card-text">0 %</p>
           </div>
-          <div class='card-footer'>
-            <small class='text-muted'>Last updated 10 mins ago</small>
+          <div class="card-footer">
+            <small class="text-muted">Last updated 10 mins ago</small>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card h-100">
+          <IconContext.Provider value={{ color: '#c54b3c' }}>
+            <VscDashboard size={100} />
+          </IconContext.Provider>
+          <div class="card-body">
+            <h5 class="card-title">Szélerősség</h5>
+            <p class="card-text">0 km/h</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Last updated 10 mins ago</small>
           </div>
         </div>
       </div>
