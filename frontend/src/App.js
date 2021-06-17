70df397fe6e47feb273fd971cbc083b91bc7fbc3
Judak Barnabas <barnajudak@gmail.com>
2021-06-17 14:04:09 +0200

    Added chart and styling @fixo91

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Dashboard from './DashBoard';
+import Chart from './Chart';
 
 function App() {
   return (
     <div>
       <Dashboard />
+      <div>
+        <Chart />
+      </div>
     </div>
   );
 }
