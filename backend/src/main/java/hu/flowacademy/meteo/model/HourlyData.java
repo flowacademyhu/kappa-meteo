22cee12d791a583a89e769b43fdb544532312da6
Vass Tamás <vass.tamas87@gmail.com>
2021-06-17 12:00:01 +0200

    basedata abstract class addded - reviewed by @Barnabas97

--- backend/src/main/java/hu/flowacademy/meteo/model/HourlyData.java
+++ backend/src/main/java/hu/flowacademy/meteo/model/HourlyData.java
@@ -3,17 +3,15 @@ package hu.flowacademy.meteo.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.NoArgsConstructor;
-import javax.persistence.*;
-import java.util.Date;
 
-@Data
+import javax.persistence.*;
+
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class HourlyData {
+public class HourlyData extends BaseData {
 
     @ManyToOne
     @JoinColumn
@@ -23,26 +21,4 @@ public class HourlyData {
     @GeneratedValue
     @JsonIgnore
     private Long id;
-
-    private Date date;
-    private Double airHumidity;
-    private Double airPressure;
-    private Double windSpeed;
-    private Double solarCellChargingVoltage;
-    private Double externalBatteryVoltage;
-    private Double irradiation;
-    private Double freeze;
-    private Double rain;
-    private Double windDirection;
-    private Double windGust;
-    private Double soilMoisture90cm;
-    private Double leafMoisture;
-    private Double soilTemperature0cm;
-    private Double airTemperature;
-    private Double internalBatteryVoltage;
-    private Double soilMoisture30cm;
-    private Double soilMoisture60cm;
-    private Double lightUnit;
-    private Double soilMoisture120cm;
-    private Double precipitationCounter;
 }
