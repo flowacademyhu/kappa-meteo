d1aec96566ce7ecb40737f49e3eb5d11053bf592
Andras92 <csikosandras3@gmail.com>
2021-06-17 20:41:53 +0200

    prettier lefutattva

--- frontend/src/Popup/MyPopup.js
+++ frontend/src/Popup/MyPopup.js
@@ -5,8 +5,12 @@ import DetailsButton from '../Button/DetailsButton';
 export default function MyPopup({ latitude, longitude, name }) {
   return (
     <Popup>
-      {name} GPS:-{latitude},-{longitude} 
-      <DetailsButton type="btn btn-outline-success m-1" onClick={console.log("")} text="Részletek"></DetailsButton>
+      {name} GPS:-{latitude},-{longitude}
+      <DetailsButton
+        type="btn btn-outline-success m-1"
+        onClick={console.log('')}
+        text="Részletek"
+      ></DetailsButton>
     </Popup>
   );
 }
