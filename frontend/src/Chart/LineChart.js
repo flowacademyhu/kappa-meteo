a1f9bc3de8260371036b159b63acd37e381596fc
nandorcsupor <csuprok@hotmail.com>
2021-06-21 11:15:48 +0200

    LineChart - start

---
+++ frontend/src/Chart/LineChart.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+
+export default function LineChart () {
+
+
+    return (
+        <>
+    <Line
+    data= {{
+        labels: ['Red', 'Blue', 'Orange']
+    }}
+    height={400}
+    width={600}
+
+
+
+
+/>
+
+
+
+        </>
+    )
+}
