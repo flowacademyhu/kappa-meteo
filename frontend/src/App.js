b3ad72eea405ea310d80b68c4c387a2bd3bbbb19
nandorcsupor <csuprok@hotmail.com>
2021-06-21 15:12:48 +0200

    LineChart progress @VassTamas87

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import Map from './Map/Map.js';
 import React from 'react';
 import Menu from './Menu.js';
+import LineChart from './Charts/LineChart.js';
 
 import {
   BrowserRouter as Router,
@@ -30,6 +31,7 @@ export default function App() {
         </div>
         <div className="m-4 p-3">
           <Map />
+        <LineChart/>
         </div>
         <Footer>Made by Buci, Barna, Fixo, Nándi, Vasi &#169;</Footer>
       </Router>
