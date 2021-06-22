313a117ba6940a626b75abc80b405e8f8457a3b8
Vass Tamás <vass.tamas87@gmail.com>
2021-06-22 15:49:27 +0200

    nav buttons hover border radius fixed - reviewed by @Barnabas97

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Map from './Map/Map.js';
 import React from 'react';
 import Menu from './Menu.js';
 
+
 import {
   BrowserRouter as Router,
   Switch,
@@ -15,28 +16,27 @@ import Footer from './Footer.js';
 export default function App() {
   return (
     <div data-testid="map-container">
-      <Router>
+      
+    <Router>
         <div>
           <nav className="navbar navbar-expand-lg bg-dark justify-content-center">
             <Menu />
-
-            <div data-testid="router" />
-          </nav>
-          <Switch>
-            <Route path="/mapview">
-              <div className="m-4 p-3">
-                <Map />
-              </div>
-            </Route>
-            <Route path="/historicdata">
-              <SwiperCards />
-            </Route>
-            <Redirect from="/" to="/mapview" />
-          </Switch>
+     
+            </nav>
+              <Switch>
+                <Route path="/mapview">
+                <div className="m-4 p-3">
+          <Map />
+          </div>
+          </Route>
+                <Route path="/historicdata" >
+                <SwiperCards/>
+                </Route>
+                <Redirect from="/" to="/mapview" />
+              </Switch>
         </div>
-
-        <Footer>Made by Buci, Barna, Fixo, Nándi, Vasi &#169;</Footer>
-      </Router>
-    </div>
+      <Footer>Made by Buci, Barna, Fixo, Nándi, Vasi &#169;</Footer>
+    </Router>
+   </div>
   );
 }
