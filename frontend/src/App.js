fdf87bbb7612ad5f37c3442a826e828ee8fc1f5b
Vass Tamás <vass.tamas87@gmail.com>
2021-06-22 16:05:32 +0200

    app.js formatting - reviewed by @Barnabas97

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,6 @@ import Map from './Map/Map.js';
 import React from 'react';
 import Menu from './Menu.js';
 
-
 import {
   BrowserRouter as Router,
   Switch,
@@ -16,27 +15,26 @@ import Footer from './Footer.js';
 export default function App() {
   return (
     <div data-testid="map-container">
-      
-    <Router>
+      <Router>
         <div>
           <nav className="navbar navbar-expand-lg bg-dark justify-content-center">
             <Menu />
-     
-            </nav>
-              <Switch>
-                <Route path="/mapview">
-                <div className="m-4 p-3">
-          <Map />
-          </div>
-          </Route>
-                <Route path="/historicdata" >
-                <SwiperCards/>
-                </Route>
-                <Redirect from="/" to="/mapview" />
-              </Switch>
+          </nav>
+          <div data-testid="router" />
+          <Switch>
+            <Route path="/mapview">
+              <div className="m-4 p-3">
+                <Map />
+              </div>
+            </Route>
+            <Route path="/historicdata">
+              <SwiperCards />
+            </Route>
+            <Redirect from="/" to="/mapview" />
+          </Switch>
         </div>
-      <Footer>Made by Buci, Barna, Fixo, Nándi, Vasi &#169;</Footer>
-    </Router>
-   </div>
+        <Footer>Made by Buci, Barna, Fixo, Nándi, Vasi &#169;</Footer>
+      </Router>
+    </div>
   );
 }
