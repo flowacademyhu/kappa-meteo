9708c308e72f8a7d12cc9db2e7a3be81759ba8f9
Vass Tamás <vass.tamas87@gmail.com>
2021-06-28 00:18:42 +0200

    data type controllers added for between 2 dates requests -@home_work

--- backend/src/main/java/hu/flowacademy/meteo/controller/MiscDataController.java
+++ backend/src/main/java/hu/flowacademy/meteo/controller/MiscDataController.java
@@ -1,12 +1,12 @@
 package hu.flowacademy.meteo.controller;
 
 import hu.flowacademy.meteo.dto.MeasurmentDto;
+import hu.flowacademy.meteo.dto.MiscDataDto;
 import hu.flowacademy.meteo.model.enumPackage.Type;
 import hu.flowacademy.meteo.service.MeasurmentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,12 +17,16 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
-public class MeasurmentController {
+public class MiscDataController {
 
     private final MeasurmentService measurmentService;
 
-    @GetMapping("latest/{id}")
-    public MeasurmentDto getLastByStationId(@PathVariable Long id) {
-        return measurmentService.getLastByStationId(id);
+    @GetMapping("misc")
+    public List<MiscDataDto> getAllBetweenDates
+            (@RequestParam("start") @DateTimeFormat(pattern = "yyyy.MM.dd. HH:mm") Date startDate,
+             @RequestParam("end") @DateTimeFormat(pattern = "yyyy.MM.dd. HH:mm") Date endDate,
+             @RequestParam("type") Type type, @RequestParam("id") Long id) throws ParseException {
+        return measurmentService.getAllBetweenDates(startDate, endDate, type, id).stream()
+                .map(MeasurmentDto::getMiscDataDto).collect(Collectors.toList());
     }
 }
