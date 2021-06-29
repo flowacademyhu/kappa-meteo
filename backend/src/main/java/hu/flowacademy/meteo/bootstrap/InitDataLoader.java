868123bb1c1fc9f5ce93e757b8c0ea41f9578b9d
Judák Barnabás <barnajudak@gmail.com>
2021-06-29 11:36:22 +0200

    Git docker test fix @VassTamas87

--- backend/src/main/java/hu/flowacademy/meteo/bootstrap/InitDataLoader.java
+++ backend/src/main/java/hu/flowacademy/meteo/bootstrap/InitDataLoader.java
@@ -81,7 +81,7 @@ public class InitDataLoader implements CommandLineRunner {
     }
 
     private String csvData(String name) {
-        return "src/main/resources/" + name;
+        return "/app/" + name;
     }
 
     private List<Measurment> populateTenMin(String name, DateFormat format, Station station, Type type) {
