29d540985edc64a64fdf57f2df144daea8dda770
Robert Fugedi <fugiss91@gmail.com>
2021-07-03 21:23:58 +0200

    Menu Flip-cards converted to styled components (Homework)

--- frontend/src/Components/SwiperMenu/CardElement.js
+++ frontend/src/Components/SwiperMenu/CardElement.js
@@ -1,27 +1,36 @@
 import { IconContext } from 'react-icons';
-import { CardFooterText, TitleText, RearTitleText } from './StyledElements';
+import {
+  RearFooterText,
+  FooterText,
+  RearTitleText,
+  FlipCardBody,
+  FlipCardInner,
+  CardFooter,
+  FlipCardFront,
+  FlipCardBack
+} from './StyledElements';
 
 export default function CardElement({ text, Icon, descript }) {
   return (
     <>
-      <div className="flip-card">
-        <div className="flip-card-inner">
-          <div className="flip-card-front">
+      <FlipCardBody className="flip-card">
+        <FlipCardInner className="flip-card-inner">
+          <FlipCardFront className="flip-card-front">
             {Icon && (
               <IconContext.Provider value={{ color: '#c54b3c' }}>
                 {<Icon size={200} />}
               </IconContext.Provider>
             )}
-            <div className="card-footer">
-              <TitleText>{text}</TitleText>
-            </div>
-          </div>
-          <div className="flip-card-back">
+            <CardFooter className="card-footer">
+              <FooterText>{text}</FooterText>
+            </CardFooter>
+          </FlipCardFront>
+          <FlipCardBack className="flip-card-back">
             <RearTitleText>Információ:</RearTitleText>
-            <CardFooterText className="card-footer">{descript}</CardFooterText>
-          </div>
-        </div>
-      </div>
+            <RearFooterText className="card-footer">{descript}</RearFooterText>
+          </FlipCardBack>
+        </FlipCardInner>
+      </FlipCardBody>
     </>
   );
 }
