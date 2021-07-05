ade34b6af1fffa2fbcd1449adeeef1654461a722
Vass Tamás <vass.tamas87@gmail.com>
2021-07-05 23:19:29 +0200

    loading component-@home_work

---
+++ frontend/src/Upload/Loading.js
@@ -0,0 +1,27 @@
+const Loading = () => (
+  <>
+    <div className="spinner-grow text-primary" role="status">
+      <span className="sr-only"></span>
+    </div>
+    <div className="spinner-grow text-secondary" role="status">
+      <span className="sr-only"></span>
+    </div>
+    <div className="spinner-grow text-success" role="status">
+      <span className="sr-only"></span>
+    </div>
+    <div className="spinner-grow text-danger" role="status">
+      <span className="sr-only"></span>
+    </div>
+    <div className="spinner-grow text-warning" role="status">
+      <span className="sr-only"></span>
+    </div>
+    <div className="spinner-grow text-info" role="status">
+      <span className="sr-only"></span>
+    </div>
+    <div className="spinner-grow text-dark" role="status">
+      <span className="sr-only"></span>
+    </div>
+  </>
+);
+
+export default Loading;
