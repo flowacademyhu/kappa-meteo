ee1ed8fd4180b0e8955dfbc66d587746572d3fe8
Robert Fugedi <fugiss91@gmail.com>
2021-07-06 14:45:17 +0200

    Prettier fix @nandorcsupor

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -4,6 +4,5 @@ import App from './App';
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByTestId('map-container');
-  f;
   expect(linkElement).toBeInTheDocument();
 });
