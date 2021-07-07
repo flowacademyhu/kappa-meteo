445a78bd2347f430d1ab7f95d6d22e9e88be39a3
Judák Barnabás <barnajudak@gmail.com>
2021-07-07 10:30:15 +0200

    Use General Chart in Air and Soil @6uliver @nandorcsupor @fixo91

--- frontend/src/Charts/SoilChart.js
+++ frontend/src/Charts/SoilChart.js
@@ -1,16 +1,6 @@
-import React, { useState } from 'react';
-import {
-  ResponsiveContainer,
-  LineChart,
-  Line,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-} from 'recharts';
-import MyCheckbox from '../Components/Input/MyCheckbox';
-import { v4 as uuidv4 } from 'uuid';
+import React from 'react';
+import GeneralChart from './GeneralChart.js';
+import { LineChart } from 'recharts';
 
 const axisLabel = [
   {
@@ -68,91 +58,14 @@ const labels = [
   },
 ];
 
-const SoilChart = ({ linedata, xAxisDateFormat }) => {
-  const [measurementGroup, setMeasurementGroup] = useState([]);
-
-  const changeMeasurement = (data) => {
-    if (measurementGroup !== null && measurementGroup !== undefined) {
-      if (measurementGroup.includes(data)) {
-        let newMeasurementGroup = measurementGroup.filter((e) => e !== data);
-        setMeasurementGroup(newMeasurementGroup);
-      } else {
-        setMeasurementGroup([...measurementGroup, data]);
-      }
-    }
-  };
-
+const SoilChart = ({ linedata }) => {
   return (
-    linedata !== null &&
-    linedata !== undefined && (
-      <>
-        <div className="row">
-          {labels.map((label, index) => (
-            <MyCheckbox
-              key={index}
-              name={label.dataKey}
-              label={label.name}
-              changeMeasurement={changeMeasurement}
-            />
-          ))}
-        </div>
-        <ResponsiveContainer width="100%" height={500}>
-          <LineChart
-            data={linedata}
-            margin={{
-              top: 25,
-              right: 60,
-              left: 40,
-              bottom: 20,
-            }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="date" tickFormatter={xAxisDateFormat} />
-            {axisLabel.map((axis, index) => {
-              if (measurementGroup.includes(axis.dataKey)) {
-                return (
-                  <YAxis
-                    key={uuidv4()}
-                    className="mx-5"
-                    yAxisId={index}
-                    orientation="left"
-                    dataKey={axis.dataKey}
-                    label={{
-                      value: axis.value,
-                      angle: -90,
-                      dx: -15,
-                      position: 'outsideLeft',
-                      stroke: axis.stroke,
-                    }}
-                  />
-                );
-              }
-              return null;
-            })}
-            <Tooltip />
-            <Legend />
-            {labels.map((label, index) => {
-              if (measurementGroup.includes(label.dataKey)) {
-                return (
-                  <Line
-                    key={index}
-                    type="monotone"
-                    dataKey={label.dataKey}
-                    name={label.name}
-                    stroke={label.stroke}
-                    activeDot={{ r: 8 }}
-                    dot={false}
-                    yAxisId={index}
-                    fill="#111"
-                  />
-                );
-              }
-              return null;
-            })}
-          </LineChart>
-        </ResponsiveContainer>
-      </>
-    )
+    <GeneralChart
+      linedata={linedata}
+      axisLabel={axisLabel}
+      labels={labels}
+      Chart={LineChart}
+    />
   );
 };
 
