5cf5d7ee65dc19d23fc63704fa83e94b1efc9381
Andras92 <csikosandras3@gmail.com>
2021-07-07 11:59:41 +0200

    deleted comments and script

--- frontend/src/Map/MarkerWithPopup.js
+++ frontend/src/Map/MarkerWithPopup.js
@@ -1,18 +1,6 @@
 import StationPopup from './StationPopup.js';
 import { Marker } from 'react-leaflet';
 import L from 'leaflet';
-// import cloudyday2 from '../Icon/AnimateIcon/cloudy-day-2.svg';
-// import cloudy from '../Icon/AnimateIcon/cloudy.svg';
-// import day from '../Icon/AnimateIcon/day.svg';
-// import rainy1 from '../Icon/AnimateIcon/rainy-1.svg';
-// import rainy6 from '../Icon/AnimateIcon/rainy-6.svg';
-
-// let icons = [cloudyday2, cloudy, day, rainy1, rainy6, day];
-
-// const iconChooser = (icons) => {
-//   let randomNum = Math.floor(Math.random() * 6);
-//   return icons[randomNum];
-// };
 
 export default function MarkerWithPopup({ station, weatherIcons, index }) {
   return (
