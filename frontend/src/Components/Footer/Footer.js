0935a1be62828e7eb42292e800c6cbc1669c622b
Robert Fugedi <fugiss91@gmail.com>
2021-07-07 12:55:31 +0200

    Prittier fix @Andras92

--- frontend/src/Components/Footer/Footer.js
+++ frontend/src/Components/Footer/Footer.js
@@ -2,11 +2,11 @@ import { FooterStyle, FooterText, FooterDescript } from './FooterElements';
 
 export default function Footer() {
   return (
-      <FooterStyle>
-        <FooterText>MeteOApp</FooterText>
-        <FooterDescript>
-          Több mint 200 moduláris állomás adatai egy helyen...
-        </FooterDescript>
-      </FooterStyle>
+    <FooterStyle>
+      <FooterText>MeteOApp</FooterText>
+      <FooterDescript>
+        Több mint 200 moduláris állomás adatai egy helyen...
+      </FooterDescript>
+    </FooterStyle>
   );
 }
--- frontend/src/Components/HeroSection/HeroElements.js
+++ frontend/src/Components/HeroSection/HeroElements.js
@@ -18,7 +18,6 @@ export const HeroBg = styled.div`
   width: 100%;
   height: 100%;
   overflow: hidden;
-  
 `;
 export const MenuWrapper = styled.div`
   text-align: center;
